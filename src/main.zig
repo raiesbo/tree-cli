5b5157148876db6283fc1fdc684ca939a5d33127
raiesbo <raiesbo@gmail.com>
2025-03-12 08:11:33 +0100

    implement first working tree diagram creation

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,30 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-
     const cwd = std.fs.cwd();
     const dir = try cwd.openDir(".", .{ .iterate = true });
 
+    try walkDir(dir, ".", 0);
+}
+
+pub fn walkDir(dir: std.fs.Dir, path: []const u8, level: u8) !void {
+    const stdout = std.io.getStdOut().writer();
+
     var it = dir.iterate();
+
     while (try it.next()) |entry| {
-        try stdout.print("File name: {s}\n", .{entry.name});
+        var indentBuffer: [128]u8 = undefined;
+        const indentLen = @min(indentBuffer.len, level * 4);
+        @memset(indentBuffer[0..indentLen], ' ');
+        try stdout.print("{s}├── {s}\n", .{ indentBuffer[0..indentLen], entry.name });
+        // TODO:
+        // try stdout.print("{s}└── {s}\n", .{ indentBuffer[0..indentLen], entry.name });
+
+        if (entry.kind == .directory and entry.name[0] != 46) {
+            var pathBuffer: [50]u8 = undefined;
+            const fullPath = try std.fmt.bufPrint(&pathBuffer, "{s}{s}{s}", .{ path, "/", entry.name });
+            const subDir = try std.fs.cwd().openDir(fullPath, .{ .iterate = true });
+            try walkDir(subDir, fullPath, level + 1);
+        }
     }
 }
