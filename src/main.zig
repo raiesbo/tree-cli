3c528a308fae67f8103aa6bd11621853a35e8678
raiesbo <raiesbo@gmail.com>
2025-03-12 13:25:40 +0100

    add elemenets final count

--- src/main.zig
+++ src/main.zig
@@ -4,10 +4,17 @@ pub fn main() !void {
     const cwd = std.fs.cwd();
     const dir = try cwd.openDir(".", .{ .iterate = true });
 
-    try walkDir(dir, ".", 0);
+    var numDirs: i32 = 0;
+    var numFiles: i32 = 0;
+
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("·\n", .{});
+
+    try walkDir(dir, ".", 0, &numDirs, &numFiles);
+    try stdout.print("\nDirectories: {} Files: {}\n", .{ numDirs, numFiles });
 }
 
-pub fn walkDir(dir: std.fs.Dir, path: []const u8, level: u8) !void {
+pub fn walkDir(dir: std.fs.Dir, path: []const u8, level: u8, numDirs: *i32, numFiles: *i32) !void {
     const stdout = std.io.getStdOut().writer();
 
     var it = dir.iterate();
@@ -25,11 +32,17 @@ pub fn walkDir(dir: std.fs.Dir, path: []const u8, level: u8) !void {
             try stdout.print("{s}└── {s}\n", .{ indentBuffer[0..indentLen], entry.name });
         }
 
-        if (entry.kind == .directory and entry.name[0] != 46) {
-            var pathBuffer: [50]u8 = undefined;
-            const fullPath = try std.fmt.bufPrint(&pathBuffer, "{s}{s}{s}", .{ path, "/", entry.name });
-            const subDir = try std.fs.cwd().openDir(fullPath, .{ .iterate = true });
-            try walkDir(subDir, fullPath, level + 1);
+        if (entry.kind == .directory) {
+            numDirs.* = numDirs.* + 1;
+
+            if (entry.name[0] != 46) {
+                var pathBuffer: [50]u8 = undefined;
+                const fullPath = try std.fmt.bufPrint(&pathBuffer, "{s}{s}{s}", .{ path, "/", entry.name });
+                const subDir = try std.fs.cwd().openDir(fullPath, .{ .iterate = true });
+                try walkDir(subDir, fullPath, level + 1, numDirs, numFiles);
+            }
+        } else {
+            numFiles.* = numFiles.* + 1;
         }
 
         currEntry = nextEntry;
