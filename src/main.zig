28d732ea3a64703aacd208b275c160244b4dcac3
raiesbo <raiesbo@gmail.com>
2025-03-13 00:16:02 +0100

    clean and simplify walkDir func

--- src/main.zig
+++ src/main.zig
@@ -10,45 +10,41 @@ pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("·\n", .{});
 
-    try walkDir(dir, ".", &numDirs, &numFiles, "");
+    try walkDir(dir, ".", "", &numDirs, &numFiles);
     try stdout.print("\nDirectories: {} Files: {}\n", .{ numDirs, numFiles });
 }
 
-pub fn walkDir(dir: std.fs.Dir, path: []const u8, numDirs: *i32, numFiles: *i32, prefix: []const u8) !void {
+pub fn walkDir(dir: std.fs.Dir, path: []const u8, prefix: []const u8, num_dirs: *i32, num_files: *i32) !void {
     const stdout = std.io.getStdOut().writer();
 
     var it = dir.iterate();
 
-    var currEntry = try it.next();
-    var nextEntry = try it.next();
+    var curr_entry = try it.next();
+    var next_entry = try it.next();
 
-    while (currEntry) |entry| {
-        if (nextEntry != null) {
-            try stdout.print("{s}├── {s}\n", .{ prefix, entry.name });
-        } else {
-            try stdout.print("{s}└── {s}\n", .{ prefix, entry.name });
-        }
+    while (curr_entry) |entry| {
+        const isLast = next_entry == null;
+
+        try stdout.print("{s}{s}── {s}\n", .{ prefix, if (isLast) "└" else "├", entry.name });
+        // TODO: Add -c // --color flag for coloring the different elements. Files / Directories
+        // try stdout.print("{s}{s}── \x1b[36m{s}\x1b[0m\n", .{ prefix, if (isLast) "└" else "├", entry.name });
 
         if (entry.kind == .directory) {
-            numDirs.* += 1;
+            num_dirs.* += 1;
 
-            var prefixBuffer: [50]u8 = undefined;
-            var newPrefix: []u8 = undefined;
-            if (nextEntry != null) {
-                newPrefix = try std.fmt.bufPrint(&prefixBuffer, "{s}│   ", .{prefix});
-            } else {
-                newPrefix = try std.fmt.bufPrint(&prefixBuffer, "{s}    ", .{prefix});
+            var new_prefix_buffer: [50]u8 = undefined;
+            const new_prefix: []u8 = try std.fmt.bufPrint(&new_prefix_buffer, "{s}{s}   ", .{ prefix, if (isLast) " " else "│" });
+
+            // TODO: Add -a to display the insides of directories that start with a "."
+            if (entry.name[0] != '.') {
+                var path_buffer: [50]u8 = undefined;
+                const full_path = try std.fmt.bufPrint(&path_buffer, "{s}{s}{s}", .{ path, "/", entry.name });
+                const sub_dir = try std.fs.cwd().openDir(full_path, .{ .iterate = true });
+                try walkDir(sub_dir, full_path, new_prefix, num_dirs, num_files);
             }
+        } else num_files.* += 1;
 
-            if (entry.name[0] != 46) {
-                var pathBuffer: [50]u8 = undefined;
-                const fullPath = try std.fmt.bufPrint(&pathBuffer, "{s}{s}{s}", .{ path, "/", entry.name });
-                const subDir = try std.fs.cwd().openDir(fullPath, .{ .iterate = true });
-                try walkDir(subDir, fullPath, numDirs, numFiles, newPrefix);
-            }
-        } else numFiles.* += 1;
-
-        currEntry = nextEntry;
-        nextEntry = try it.next();
+        curr_entry = next_entry;
+        next_entry = try it.next();
     }
 }
