89e5f03a53088013f51fbe61c6a8ba6d6ed28706
raiesbo <raiesbo@gmail.com>
2025-03-19 14:15:27 +0100

    fix panic when entry names are wrongly formatted

--- src/tree.zig
+++ src/tree.zig
@@ -35,6 +35,13 @@ pub const Tree = struct {
         var next_entry = try it.next();
 
         while (curr_entry) |entry| {
+            // Saveguard to avoid conflicting entry names
+            if (std.mem.indexOfScalar(u8, entry.name, 0) != null) {
+                curr_entry = next_entry;
+                next_entry = try it.next();
+                continue;
+            }
+
             const is_last_entry = next_entry == null;
 
             try stdout.print("{s}{s}── \x1b[{s}m{s}\x1b[0m\n", .{
@@ -50,8 +57,7 @@ pub const Tree = struct {
                 if (t.with_hidden_dirs or entry.name[0] != '.') {
                     const new_prefix: []u8 = try std.fmt.allocPrint(alloc, "{s}{s}   ", .{ prefix, if (is_last_entry) " " else "│" });
 
-                    // TODO: Validate path to see if the directory still exists before attempting to open it.
-                    const sub_dir = try dir.openDir(entry.name, .{ .iterate = true });
+                    const sub_dir = dir.openDir(entry.name, .{ .iterate = true }) catch return;
                     try t.walkDirsAux(sub_dir, new_prefix, alloc);
                 }
             } else num_files += 1;
